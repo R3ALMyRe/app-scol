bd5cab08cb3aafe828076c1e24a05c7948456dbc
Remy <myre@gmail.com>
2024-03-21 23:43:46 +0100

    fini

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { EtudiantComponent } from './components/etudiant/etudiant.component';
 import { SpecialiteComponent } from './components/specialite/specialite.component';
 import { EtudiantParisComponent } from './components/etudiant-paris/etudiant-paris.component';
 import { EtudiantHorsParisComponent } from './components/etudiant-hors-paris/etudiant-hors-paris.component';
-
+import { FormEtudiantComponent } from './forms/form-etudiant/form-etudiant.component';
+import { FormSpecialiteComponent } from './forms/form-specialite/form-specialite.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'etudiant', component: EtudiantComponent },
   { path: 'specialite', component: SpecialiteComponent },
   { path: 'etudiant-paris', component: EtudiantParisComponent },
   { path: 'etudiant-hors-paris', component: EtudiantHorsParisComponent },
+  { path: 'form-etudiant', component: FormEtudiantComponent },
+  { path: 'form-specialite', component: FormSpecialiteComponent },
 
 ];
 
--- src/app/forms/form-etudiant/form-etudiant.component.ts
+++ src/app/forms/form-etudiant/form-etudiant.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EtudiantServiceService } from '../../services/etudiant-service.service';
+import { IEtudiant } from '../../models/ietudiant';
 
 @Component({
   selector: 'app-form-etudiant',
   templateUrl: './form-etudiant.component.html',
-  styles: ``
+  styleUrls: ['./form-etudiant.component.css']
 })
-export class FormEtudiantComponent {
+export class FormEtudiantComponent implements OnInit {
+  RechercheValeur: string = '';
+  etudiants: IEtudiant[] = [];
+  filteredEtudiants: IEtudiant[] = [];
 
+  constructor(private etudiantService: EtudiantServiceService) { }
+
+  ngOnInit() {
+    this.etudiants = this.etudiantService.getEtudiants();
+    this.filteredEtudiants = [];
+  }
+
+  onFormSubmit(event: Event) {
+    event.preventDefault();
+    if (this.RechercheValeur && this.RechercheValeur.trim() !== '') {
+      this.filteredEtudiants = this.etudiants.filter(etudiant =>
+        etudiant._nom.includes(this.RechercheValeur) ||
+        etudiant._prenom.includes(this.RechercheValeur) ||
+        etudiant._rue.includes(this.RechercheValeur) ||
+        etudiant._cp.includes(this.RechercheValeur) ||
+        etudiant._ville.includes(this.RechercheValeur) ||
+        etudiant._courriel.includes(this.RechercheValeur) ||
+        etudiant._genre.includes(this.RechercheValeur) ||
+        etudiant._age.toString().includes(this.RechercheValeur) ||
+        etudiant._idCand.toString().includes(this.RechercheValeur)
+      );
+    } else {
+      this.filteredEtudiants = this.etudiants;
+    }
+  }
 }
